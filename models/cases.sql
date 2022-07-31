3d0e90b52342b0acc580ab92ab7221376e4a4dd2
Florian Fesseler <florian.fesseler@shipup.co>
2022-07-31 17:05:43 +0200

    update project ID in cases.sql

--- models/cases.sql
+++ models/cases.sql
@@ -1,1 +1,1 @@
-select * from `modern-data-stack-demo.demo_dataset.covid_data` where new_recovered > new_confirmed
+select * from `mindful-origin-358014.demo_dataset.covid_data` where new_recovered > new_confirmed
